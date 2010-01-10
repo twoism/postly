23738ea7cb3e301d4536bee0293b10c751334404
twoism <signalstatic@gmail.com>
2010-01-09 19:59:05 -0500

    refactoring

--- lib/posterous/connection.rb
+++ lib/posterous/connection.rb
@@ -10,15 +10,21 @@ module Posterous
     # Some twisted injects to conform the ugly 
     # XML into pretty little objects.
     def self.parse_nodes response
-      class_name = self.to_s.split("::").last
-      site_nodes = Nokogiri::HTML(response.body).css(class_name.downcase)
-      site_nodes.inject([]) do |site_array,site_node|
+      self.nodes_for(response).inject([]) do |site_array,site_node|
         site_array << site_node.children.inject(self.new) do |site,c|
           returning site do
             site.send "#{c.name.to_sym}=", c.text if site.respond_to? "#{c.name.to_sym}=" 
           end
         end
       end
+    end
+    
+    def self.nodes_for response
+      Nokogiri::HTML(response.body).css(self.class_name.downcase)
+    end
+    
+    def self.class_name
+      @class_name ||= self.to_s.split("::").last
     end
     
   end
