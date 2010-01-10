de057273dc02d2e10bac6e1e51f5b7690413bc56
twoism <signalstatic@gmail.com>
2010-01-10 00:38:35 -0500

    #

--- lib/posterous/site.rb
+++ lib/posterous/site.rb
@@ -6,9 +6,7 @@ module Posterous
   # Returns a collection of the authenticated user's Sites
   class Site < Connection
     
-    attr_accessor :name, :url, :private, :primary, :hostname
-    attr_accessor :commentsenabled, :num_posts, :id
-
+    attr_accessor         *SITE_ATTRS
     define_remote_method  :all, :path => '/getsites', :on_success => lambda { | response | parse_nodes response.body }
     
   end
