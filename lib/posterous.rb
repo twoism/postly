373f9f68b859677bd3fca76c1d669fd97c037d0f
twoism <signalstatic@gmail.com>
2010-01-10 15:21:21 -0500

    updating docs

--- lib/posterous.rb
+++ lib/posterous.rb
@@ -5,7 +5,7 @@ $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 module Posterous
   
   SITE_ATTRS = [:name,    :url,   :private, :primary,   :hostname,  :commentsenabled, :num_posts, :id]
-  POST_ATTRS = [:site_id, :title, :body,    :autopost,  :private,   :date,            :tags,      :source, :sourceLink]
+  POST_ATTRS = [:site_id, :title, :body,    :autopost,  :private,   :date,            :tags,      :source, :sourceLink, :id]
   
   # defunkt's singleton trick
   extend self
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,21 +13,27 @@ end
 @dir          = File.dirname(__FILE__) + '/fixtures'
 @wp_xml       = File.open("#{@dir}/wp.xml", 'r')
 
-site  = Posterous::Site.find.last
-doc   = Nokogiri::HTML @wp_xml.read
-
-doc.css("item").each do |item|
-  item.css("encoded img").each { |img| img["class"] = "posterous_download_image" }
-  title = item.css("title").text
-  body  = item.css("encoded").to_s
-  params = {
-    :site_id => site.id,
-    :title   => title,
-    :body    => CGI.unescapeHTML(body).gsub(/<encoded>|<\/encoded>|\]\]>/,"")
-  }
-  post = Posterous::Post.create(params)
-  puts post.inspect
-end
+# site  = Posterous::Site.find.last
+# doc   = Nokogiri::HTML @wp_xml.read
+# 
+# doc.css("item").each do |item|
+#   item.css("encoded img").each { |img| img["class"] = "posterous_download_image" }
+#   title = item.css("title").text
+#   body  = item.css("encoded").to_s
+#   params = {
+#     :site_id => site.id,
+#     :title   => title,
+#     :body    => CGI.unescapeHTML(body).gsub(/<encoded>|<\/encoded>|\]\]>/,"")
+#   }
+#   post = Posterous::Post.create(params)
+#   puts post.inspect
+# end
 
 # posterous_download_image
 # http://farm3.static.flickr.com/2407/2243020634_9508a65fe3.jpg
+
+
+my_sites = Posterous::Site.find
+post  = Posterous::Post.create(:site_id => my_sites.last.id, :title => "New API Post", :body => "Post body")
+puts post.inspect
+#puts Posterous::Post.update(post.id, :title => "New Title", :body => "New Body").inspect
