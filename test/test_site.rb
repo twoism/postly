4d206ab75dc41fe262683b9789b32c38dbb630be
twoism <signalstatic@gmail.com>
2010-01-10 16:31:48 -0500

    Blog importing + updated docs

--- test/test_site.rb
+++ test/test_site.rb
@@ -28,48 +28,6 @@ XML
           assert Site.new.respond_to? method
         end
     end
-    
-    context "#all" do
-      should "responds to" do
-        assert Site.respond_to? :all
-      end
-    end
-    
-    context "#class_name" do
-      should "eql Site" do
-        assert_equal("Site", Site.class_name)
-      end
-    end
-    
-    context "#nodes_for" do
-      should "eql 1" do
-        assert_equal 1, Site.nodes_for(@site_response).count
-      end
-    end
-    
-    context "#parse_nodes" do
-      setup do
-        @parsed = Site.parse_nodes(@site_response)
-      end
-      
-      should "return an array" do
-        assert @parsed.is_a? Array
-      end
-      
-      should "have 1 result" do
-        assert_equal 1, @parsed.count
-      end
-      
-      context "returned contents" do 
-        setup do
-          @site = @parsed.first
-        end
 
-        should "be Sites" do
-          assert @site.is_a? Site
-        end
-      end
-      
-    end
   end
 end
