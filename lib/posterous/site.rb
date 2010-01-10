e1aa08e2b2a49a440020829caace3a8c585f6d10
Christopher Burnett <signalstatic@gmail.com>
2010-01-09 22:12:39 -0700

    method style

--- lib/posterous/site.rb
+++ lib/posterous/site.rb
@@ -9,9 +9,7 @@ module Posterous
     attr_accessor :name, :url, :private, :primary, :hostname
     attr_accessor :commentsenabled, :num_posts, :id
 
-    define_remote_method  :all, 
-                          :path => '/getsites', 
-                          :on_success => lambda { | response | parse_nodes response.body }
+    define_remote_method  :all, :path => '/getsites', :on_success => lambda { | response | parse_nodes response.body }
     
   end
   
